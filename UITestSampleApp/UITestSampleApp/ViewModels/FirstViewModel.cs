1faa452aafc0453c94a1b1909036530d93533011
brminnick <brandon.r.minnick@gmail.com>
2017-05-12 14:29:23 -0700

    Removed C#7 features
    BitRise builds failing because BitRise doesn’t support C#7 yet

--- UITestSampleApp/UITestSampleApp/ViewModels/FirstViewModel.cs
+++ UITestSampleApp/UITestSampleApp/ViewModels/FirstViewModel.cs
@@ -6,53 +6,53 @@ using Xamarin.Forms;
 
 namespace UITestSampleApp
 {
-	public class FirstViewModel : BaseViewModel
-	{
-		#region Fields
-		bool _isActivityIndicatorRunning;
-		string _entryText, _labelText;
-		ICommand _goButtonTappedCommand;
-		#endregion
+    public class FirstViewModel : BaseViewModel
+    {
+        #region Fields
+        bool _isActivityIndicatorRunning;
+        string _entryText, _labelText;
+        ICommand _goButtonTappedCommand;
+        #endregion
 
-		#region Properties
-		public bool IsActiityIndicatorRunning
-		{
-			get => _isActivityIndicatorRunning;
-			set => SetProperty(ref _isActivityIndicatorRunning, value);
-		}
+        #region Properties
+        public bool IsActiityIndicatorRunning
+        {
+            get { return _isActivityIndicatorRunning; }
+            set { SetProperty(ref _isActivityIndicatorRunning, value); }
+        }
 
-		public string EntryText
-		{
-			get => _entryText;
-			set => SetProperty(ref _entryText, value);
-		}
+        public string EntryText
+        {
+            get { return _entryText; }
+            set { SetProperty(ref _entryText, value); }
+        }
 
-		public string LabelText
-		{
-			get => _labelText;
-			set => SetProperty(ref _labelText, value);
-		}
+        public string LabelText
+        {
+            get { return _labelText; }
+            set { SetProperty(ref _labelText, value); }
+        }
 
-		public ICommand GoButtonTappedCommand =>
+        public ICommand GoButtonTappedCommand =>
             _goButtonTappedCommand ??
-		        (_goButtonTappedCommand = new Command(async () => await ExecuteGoButtonTapped()));
-		#endregion
+                (_goButtonTappedCommand = new Command(async () => await ExecuteGoButtonTapped()));
+        #endregion
 
-		#region Methods
-		async Task ExecuteGoButtonTapped()
-		{
-			MobileCenterHelpers.TrackEvent(MobileCenterConstants.GoButtonTapped, new Dictionary<string, string> {
-				{ MobileCenterConstants.FirstPageTextEntered, EntryText }
-			});
+        #region Methods
+        async Task ExecuteGoButtonTapped()
+        {
+            MobileCenterHelpers.TrackEvent(MobileCenterConstants.GoButtonTapped, new Dictionary<string, string> {
+                { MobileCenterConstants.FirstPageTextEntered, EntryText }
+            });
 
-			IsActiityIndicatorRunning = true;
+            IsActiityIndicatorRunning = true;
 
-			await Task.Delay(2000);
+            await Task.Delay(2000);
 
-			IsActiityIndicatorRunning = false;
-			LabelText = EntryText;
-		}
+            IsActiityIndicatorRunning = false;
+            LabelText = EntryText;
+        }
 
-		#endregion
-	}
+        #endregion
+    }
 }
--- UITestSampleApp/UITestSampleApp/ViewModels/ListViewModel.cs
+++ UITestSampleApp/UITestSampleApp/ViewModels/ListViewModel.cs
@@ -10,99 +10,99 @@ using Plugin.Connectivity;
 
 namespace UITestSampleApp
 {
-	public class ListViewModel : BaseViewModel
-	{
-		#region Fields
-		bool _isDataLoadingFromBackend;
+    public class ListViewModel : BaseViewModel
+    {
+        #region Fields
+        bool _isDataLoadingFromBackend;
 
-		ICommand _PullToRefreshCommand;
+        ICommand _PullToRefreshCommand;
 
-		List<ListPageDataModel> _dataList;
-		#endregion
+        List<ListPageDataModel> _dataList;
+        #endregion
 
-		#region Constructors
-		public ListViewModel()
-		{
-			Task.Run(async () => await RefreshDataAsync());
-		}
-		#endregion
+        #region Constructors
+        public ListViewModel()
+        {
+            Task.Run(async () => await RefreshDataAsync());
+        }
+        #endregion
 
-		#region Events
-		public event EventHandler LoadingDataFromBackendCompleted;
-		#endregion
+        #region Events
+        public event EventHandler LoadingDataFromBackendCompleted;
+        #endregion
 
-		#region Properties
-		public List<ListPageDataModel> DataList
-		{
-			get => _dataList;
-			set => SetProperty(ref _dataList, value);
-		}
+        #region Properties
+        public List<ListPageDataModel> DataList
+        {
+            get { return _dataList; }
+            set { SetProperty(ref _dataList, value); }
+        }
 
-		public bool IsDataLoading
-		{
-			get => _isDataLoadingFromBackend;
-			set => SetProperty(ref _isDataLoadingFromBackend, value);
-		}
+        public bool IsDataLoading
+        {
+            get { return _isDataLoadingFromBackend; }
+            set { SetProperty(ref _isDataLoadingFromBackend, value); }
+        }
 
-		public ICommand PullToRefreshCommand => _PullToRefreshCommand ??
-			(_PullToRefreshCommand = new Command(async () => await ExecutePullToRefreshCommanded()));
-		#endregion
+        public ICommand PullToRefreshCommand => _PullToRefreshCommand ??
+            (_PullToRefreshCommand = new Command(async () => await ExecutePullToRefreshCommanded()));
+        #endregion
 
-		#region Methods
-		async Task RefreshDataFromAzureAsync()
-		{
-			var isAzureDatabaseReachable = await CrossConnectivity.Current.IsRemoteReachable(AzureConstants.AzureDataServiceUrl, 80, 1000);
-			if (!CrossConnectivity.Current.IsConnected || !isAzureDatabaseReachable)
-				return;
+        #region Methods
+        async Task RefreshDataFromAzureAsync()
+        {
+            var isAzureDatabaseReachable = await CrossConnectivity.Current.IsRemoteReachable(AzureConstants.AzureDataServiceUrl, 80, 1000);
+            if (!CrossConnectivity.Current.IsConnected || !isAzureDatabaseReachable)
+                return;
 
-			try
-			{
-				await DependencyService.Get<IDataService>().SyncItemsAsync<ListPageDataModel>();
-				var dataListAsIEnumerable = await DependencyService.Get<IDataService>().GetItemsAsync<ListPageDataModel>();
-				DataList = dataListAsIEnumerable.ToList();
-			}
-			catch (Exception e)
-			{
-				MobileCenterHelpers.Log("Error Retrieving Data From Azure", e.Message, e);
-			}
-		}
+            try
+            {
+                await DependencyService.Get<IDataService>().SyncItemsAsync<ListPageDataModel>();
+                var dataListAsIEnumerable = await DependencyService.Get<IDataService>().GetItemsAsync<ListPageDataModel>();
+                DataList = dataListAsIEnumerable.ToList();
+            }
+            catch (Exception e)
+            {
+                MobileCenterHelpers.Log("Error Retrieving Data From Azure", e.Message, e);
+            }
+        }
 
-		async Task RefreshDataFromLocalDatabaseAsync()
-		{
-			try
-			{
-				var dataListAsIEnumerable = await DependencyService.Get<IDataService>().GetItemsAsync<ListPageDataModel>();
-				DataList = dataListAsIEnumerable?.ToList();
-			}
-			catch (Exception e)
-			{
-				MobileCenterHelpers.Log("Error Retriving Data From Local Database", e.Message, e);
-			}
-		}
+        async Task RefreshDataFromLocalDatabaseAsync()
+        {
+            try
+            {
+                var dataListAsIEnumerable = await DependencyService.Get<IDataService>().GetItemsAsync<ListPageDataModel>();
+                DataList = dataListAsIEnumerable?.ToList();
+            }
+            catch (Exception e)
+            {
+                MobileCenterHelpers.Log("Error Retriving Data From Local Database", e.Message, e);
+            }
+        }
 
-		async Task ExecutePullToRefreshCommanded()
-		{
-			MobileCenterHelpers.TrackEvent(MobileCenterConstants.PullToRefreshCommanded);
+        async Task ExecutePullToRefreshCommanded()
+        {
+            MobileCenterHelpers.TrackEvent(MobileCenterConstants.PullToRefreshCommanded);
 
-			await RefreshDataAsync();
+            await RefreshDataAsync();
 
-			OnLoadingDataFromBackendCompleted();
-		}
+            OnLoadingDataFromBackendCompleted();
+        }
 
-		async Task RefreshDataAsync()
-		{
-			IsDataLoading = true;
+        async Task RefreshDataAsync()
+        {
+            IsDataLoading = true;
 
-			await RefreshDataFromLocalDatabaseAsync();
-			await RefreshDataFromAzureAsync();
+            await RefreshDataFromLocalDatabaseAsync();
+            await RefreshDataFromAzureAsync();
 
-			OnLoadingDataFromBackendCompleted();
+            OnLoadingDataFromBackendCompleted();
 
-			IsDataLoading = false;
-		}
+            IsDataLoading = false;
+        }
 
-		void OnLoadingDataFromBackendCompleted() =>
-			LoadingDataFromBackendCompleted?.Invoke(null, EventArgs.Empty);
-		#endregion
-	}
+        void OnLoadingDataFromBackendCompleted() =>
+            LoadingDataFromBackendCompleted?.Invoke(null, EventArgs.Empty);
+        #endregion
+    }
 }
